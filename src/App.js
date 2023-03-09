a5e50eb8d20a188467aac9c23019364680ed2e59
Enes Talha Uçar <enestalha0208@gmail.com>
2023-03-09 18:06:43 +0300

    Firebase Entegrated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import { register } from "./firebase";
+import { useState } from "react";
 
 function App() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async e => {
+    e.prevent.default()
+    const user = await register(email, password)
+    console.log(user);
+  }
+
   return (
     <div>
-      Hello
+      <form>
+        <input type="text" placeholder="Enter Email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" placeholder="Enter Password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
     </div>
   );
 }
